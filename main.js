cb7c43751b62e86d5254e884c91053a542c1124d
Tym17 <Tym17@users.noreply.github.com>
2018-10-25 19:18:39 +0200

    added more config

--- main.js
+++ main.js
@@ -3,20 +3,47 @@ let http = require('http').Server(app);
 let io = require('socket.io')(http);
 const config = require('./config.json');
 
+/**
+ * Routes
+ */
+
+/**
+ * @api {get} / Root
+ * @apiDescription Test if pusher is running
+ * @apiName Test
+ * @apiGroup General
+ * 
+ */
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/resources/index.html');
 });
 
+/**
+ * @api {get} /status Status
+ * @apiDescription Returns informations about the pusher
+ * @apiName Status
+ * @apiGroup General
+ */
 app.get('/status', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.json({ok: true});
 });
 
 if (config.debugmode) {
+    /**
+     * @api {get} /debug DebugConsole
+     * @apiDescription Shows debug console which can be used to manually trigger events
+     * @apiName DebugConsole
+     * @apiGroup Debug
+     */
     app.get('/debug', function(req, res) {
         res.sendFile(__dirname + '/resources/debug.html');
     })
 }
+
+/**
+ * Sockets
+ */
 
 io.on('connection', socket => {
     console.log('Somebody connected');
@@ -31,6 +58,9 @@ io.on('connection', socket => {
     });
 });
 
-http.listen(7000, () => {
-    console.log('Listening on *:7000');
+/**
+ * Initialization
+ */
+http.listen(config.port, () => {
+    console.log(`Listening on *:${config.port}`);
 });
